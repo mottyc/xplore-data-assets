0905cb883a62c07e796a76f3e207dbcedda1e747
Motty Cohen <motty@artrunners.com>
2016-09-04 11:27:57 +0300

    Add card view layouts

--- xplore-assets-ui/grunt_serve.sh
+++ xplore-assets-ui/grunt_serve.sh
@@ -2,19 +2,12 @@
 # Build frontend single page application using grunt build tool
 
 WORK_DIR=$1
-COPY_DIR=$2
-TASK=$3
+TASK=$2
 
 cd $WORK_DIR
 
 echo "grunt " $WORK_DIR $TASK
 grunt $TASK
-
-echo "copy " $WORK_DIR " -> " $TCOPY_DIR
-
-
-# Copy the client side SPA to the WAR webapp folder for packaging
-cp -R $WORK_DIR/dist/. $COPY_DIR
 
 # Display Done
 echo "Done"
